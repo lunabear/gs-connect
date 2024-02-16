384ffc7ab45edf7e057bfda5c6f533eabb0b8e99
Bob 박종혁 <dongom910@gmail.com>
2024-02-16 09:36:46 +0900

    init

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def run():
             with cent_co:
                 st.image('assets/52g_logo.png', width=200)
                 st.title("Login")
-                email = st.text_input('Email', )
-                pw = st.text_input('Password', type='password')
+                email = st.text_input('Email', value='bob@gs.co.kr')
+                pw = st.text_input('Password', value='Lunabear910!', type='password')
 
                 with st.form("my_form"):
                     submitted = st.form_submit_button("Submit")
@@ -84,11 +84,23 @@ def run():
             with st.chat_message("user"):
                 st.markdown(prompt)
                 st.session_state["app_id"] = st.session_state['app_map_dict'][st.session_state["selected_app_name"]]
-                response = ask(prompt, st.session_state['current_thread'], st.session_state["app_id"])
+
+
             with st.chat_message("assistant"):
-                st.markdown(response)
-                st.session_state[st.session_state['current_thread']].append(
-                    {"role": "assistant", "content": response})
+                with st.spinner("많이 답답하시지요? 저희가 답변을 만들고 있어요."):
+                    response = ask(prompt, st.session_state['current_thread'], st.session_state["app_id"])
+                    print(response)
+                    content = response['data']['result']
+                    # st.markdown(content)
+                    def fake_streaming(content):
+                        content = f'({st.session_state["selected_app_name"]}) ' + content
+                        import time
+                        for word in content.split():
+                            yield word + " "
+                            time.sleep(0.1)
+
+                st.write_stream(fake_streaming(content))
+                st.session_state[st.session_state['current_thread']].append({"role": "assistant", "content": content})
 
 
 if __name__ == "__main__":
